c1e7f8c0bbcead324b5d84892008927f730b5c57
Kara Jelley <karajelley@gmail.com>
2024-11-18 10:37:25 -0500

    details page edits

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,7 +1,9 @@
 import { useParams } from "react-router-dom";
-import { NavLink } from "react-router-dom";
-import { useState } from "react";
 import { Link } from "react-router-dom";
+import supabase from "../supabase/config";
+import { useNavigate } from "react-router-dom";
+import deleteIcon from "../assets/delete.svg";
+import "./DetailsPage.css"
 
 function DetailsPage({moviesArray}){
 
@@ -12,7 +14,23 @@ function DetailsPage({moviesArray}){
    // console.log("this is the params id:" , id)
 
     const movie = moviesArray.find((movieItem) => movieItem._id === id);
-    //console.log(movie)
+    console.log(movie)
+
+    const navigate = useNavigate();
+    async function deleteItem(id) {
+    try {
+      const resp = await supabase.from("moviesdb").delete().eq("_id", id);
+      console.log(resp);
+      navigate("/allmovies");
+    } catch {
+      console.log("There's been an error deleting an item:");
+    }
+  }
+
+    //format the genre array
+    const formattedGenre = movie.genre && movie.genre.length > 0 
+    ? movie.genre.join(", ") 
+    : ""; 
 
     return (
         <>
@@ -23,9 +41,39 @@ function DetailsPage({moviesArray}){
                     </Link>
                     <h2>{movie.title}</h2>
 
-                    <Link to={`/movie/${id}/editmovie`}>
-                        <button>Edit</button>
-                    </Link>
+                </div>
+                <div className="left-side-content">
+
+                    <div className="quick-details">
+                    <h4>{movie.watched ? '✅ Watched' : '🙈 Unwatched'}</h4>
+                    <h4>🎭 Genre: {formattedGenre}</h4>
+                    <h4>🍅 Rotton Tomatoes: {movie.rotten_tomatoes}%</h4>
+                    <h4>⭐️ Audience Rating: {movie.audience_rating*10}%</h4>
+                    </div>
+
+                    <div className="lists">
+                        <h4>Lists: </h4>
+                    </div>
+
+                    <div className="description">
+                        <h4>Description:</h4>
+                        <p>{movie.description}</p>
+                    </div>
+
+                    <div className="right-side-content">
+                        <Link to={`/movie/${id}/editmovie`}>
+                        <button
+                            onClick={() => {
+                                deleteItem(movie._id);
+                            }}
+                        >
+                            <img src={deleteIcon} alt="delete icon" />
+                        </button>
+                            <button>Edit</button>
+                        </Link>
+
+                        <img src={movie.image} alt={movie.title} />
+                    </div>
                     
             </div>
             </section>
